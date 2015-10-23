d5b5779af94a75b98e89ee6e18a4c4edd0490eea
mtnewton <mtnewton3@gmail.com>
2015-10-23 17:15:44 -0400

    fixed search error

--- HelpingHands.js
+++ HelpingHands.js
@@ -1,3 +1,10 @@
+if (Meteor.isServer) {
+	Organization = new Meteor.Collection('organization');
+    Events = new Meteor.Collection('events');
+	Meteor.startup(function () {
+		// code to run on server at startup
+	});
+}
 if (Meteor.isClient) {
 	
 	Template.socialShareBasic.helpers({
@@ -12,10 +19,10 @@ if (Meteor.isClient) {
 		  };
 		  return opts;
 		}
-	  });
+	});
 	  
 	  
-	 Template.lmSocialShare.rendered = function(){
+	Template.lmSocialShare.rendered = function(){
 		var inner = $('.lm-social-share-facebook').html();
 		var icon = "<i class='fa fa-facebook-square'></i>";
 		$('.lm-social-share-facebook').html(icon + inner);
@@ -24,96 +31,100 @@ if (Meteor.isClient) {
 		var icon = "<i class='fa fa-twitter-square'></i>";
 		$('.lm-social-share-twitter').html(icon + inner);
 	 };
-  
-	oldInput = "";
-	currentFilter = "name";	//1 - name, 2 - date+time, 3 - location, 4 - tag list
-	Organization = new Meteor.Collection('organization');
-        Events = new Meteor.Collection('events');
+	 
+	Meteor.startup(function(){
+		$("#user_search").on("input", inputSearch);
+		document.getElementById("search_submit").addEventListener("click", function(){alert("click");});
+		$("#search_submit").on("click", inputSearch);
 	
-	
-	function inputSearch() {
-		if(oldInput == $("#user_search")[0].value || $("#user_search")[0].value == "")
-			return 1;
 		
-		//Clear results
-		while($("#search_results").firstChild)
-			$("#search_results").removeChild($("#search_results"));
 		
-		switch(currentFilter) {
-			case "name":
-				var searchObj = {organization:$("#user_search")[0].value}
-				break;
-			case "date":
-				var searchObj = {date:getUserDate()}
-				break;
-			case "location":
-				var searchObj = {location:$("#user_search")[0].value}
-				break;
-			case "tag list":
-				var searchObj = {tag_list:$("#user_search")[0].value}
-				break;
+	  
+		
+		oldInput = "";
+		currentFilter = "name";	//1 - name, 2 - date+time, 3 - location, 4 - tag list
+		Organization = new Meteor.Collection('organization');
+		Events = new Meteor.Collection('events');
+		
+		
+		function inputSearch() {
+			console.log("GASDJKBFLABSJKN:DNABLSHBJ:DNASIBFFK WORKS")
+			if(oldInput == $("#user_search")[0].value || $("#user_search")[0].value == "")
+				return 1;
+			
+			//Clear results
+			while($("#search_results").firstChild)
+				$("#search_results").removeChild($("#search_results"));
+			
+			switch(currentFilter) {
+				case "name":
+					var searchObj = {organization:$("#user_search")[0].value}
+					break;
+				case "date":
+					var searchObj = {date:getUserDate()}
+					break;
+				case "location":
+					var searchObj = {location:$("#user_search")[0].value}
+					break;
+				case "tag list":
+					var searchObj = {tag_list:$("#user_search")[0].value}
+					break;
+			}
+			var results = Events.find(searchObj).fetch();
+			if(typeof results == "undefined" || results == null) {
+				$("#search_results")[0].innerHTML = "<h3>No Results!</h3>";
+			} else {
+				results.forEach(insertEventResult);
+			}
+			
+			oldInput = $("#user_search")[0].value;
+			return 0;
 		}
-		var results = Events.find(searchObj).fetch();
-		if(typeof results == "undefined" || results == null) {
-			$("#search_results")[0].innerHTML = "<h3>No Results!</h3>";
-		} else {
-			results.forEach(insertEventResult);
+		
+		
+		/*Processes input field and converts it to a string for the database
+		*args: none
+		*return: string for database search*/
+		function getUserDate() {
+			var udate = $("#search_field")[0].value;
+			//TODO - convert user's string (e.g. "December 15") to database formatted time (e.g. 17263275482)
+			return udate;
 		}
 		
-		oldInput = $("#user_search")[0].value;
-		return 0;
-	}
-	$("#user_search").on("input", inputSearch);
-	document.getElementById("search_submit").addEventListener("click", function(){alert("click");});
-	$("#search_submit").on("click", inputSearch);
-	
-	/*Processes input field and converts it to a string for the database
-	*args: none
-	*return: string for database search*/
-	function getUserDate() {
-		var udate = $("#search_field")[0].value;
-		//TODO - convert user's string (e.g. "December 15") to database formatted time (e.g. 17263275482)
-		return udate;
-	}
-	
-	/*Extract data from argument, apply HTML formatting, add to "search_results" element
-	*args: array of events
-	*return: none*/
-	function insertEventResult(entry, index, arr) {
-		var evt_name = entry.name,
-		evt_date = entry.date,
-		evt_city = entry.city,
-		evt_state = entry.state;
+		/*Extract data from argument, apply HTML formatting, add to "search_results" element
+		*args: array of events
+		*return: none*/
+		function insertEventResult(entry, index, arr) {
+			var evt_name = entry.name,
+			evt_date = entry.date,
+			evt_city = entry.city,
+			evt_state = entry.state;
+			
+			//method 1
+			var entryHTML = "<div id=\"result"+(index+1)+"\" class=\"result_entry\">"+
+			"<span name=\"name\">"+evt_name+"</span>"+
+			"<span name=\"date\">"+evt_date+"</span>"+
+			"<span name=\"city\">"+evt_city+"</span>"+
+			"<span name=\"state\">"+evt_state+"</span>"+
+			"</div><br/>";
+			$("#search_results")[0].innerHTML += entryHTML;
+			
+			//method 2
+			/*var new_result = document.createElement("div");
+			new_result.id = "result"+(index+1);
+			new_result.className = "result_entry";
+			$("#search_results").appendChild(new_result);*/
+			
+			//console.log("new entry html: " + entryHTML);
+		}
 		
-		//method 1
-		var entryHTML = "<div id=\"result"+(index+1)+"\" class=\"result_entry\">"+
-		"<span name=\"name\">"+evt_name+"</span>"+
-		"<span name=\"date\">"+evt_date+"</span>"+
-		"<span name=\"city\">"+evt_city+"</span>"+
-		"<span name=\"state\">"+evt_state+"</span>"+
-		"</div><br/>";
-		$("#search_results")[0].innerHTML += entryHTML;
-		
-		//method 2
-		/*var new_result = document.createElement("div");
-		new_result.id = "result"+(index+1);
-		new_result.className = "result_entry";
-		$("#search_results").appendChild(new_result);*/
-		
-		//console.log("new entry html: " + entryHTML);
-	}
+		populate = function() {
+			//var tmp_search = {name:"TestOne"};
+			var tmp_result = Events.find("One").fetch();
+			tmp_result.forEach(insertEventResult);
+		}
 	
-	populate = function() {
-		//var tmp_search = {name:"TestOne"};
-		var tmp_result = Events.find("One").fetch();
-		tmp_result.forEach(insertEventResult);
-	}
+	})
 }
 
-if (Meteor.isServer) {
-	Organization = new Meteor.Collection('organization');
-        Events = new Meteor.Collection('events');
-	Meteor.startup(function () {
-		// code to run on server at startup
-	});
-}
+
