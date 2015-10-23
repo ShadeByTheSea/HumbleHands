69fad4b889812c56b5af1ad5392f82628a8c933d
matthew.tiffany <matthew.tiffany@dominionenterprises.com>
2015-10-23 11:39:37 -0400

    added organizations

--- HelpingHands.js
+++ HelpingHands.js
@@ -1,23 +1,11 @@
+Organization = new Meteor.Collection('organization');
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
 }
 
 if (Meteor.isServer) {
-  Organization = new Meteor.Collection('organization');
   Meteor.startup(function () {
     // code to run on server at startup
   });
